0583e926d9dcbf665535b8e3d012b037029e4a49
Molly <117697419+learningcoding2022@users.noreply.github.com>
2025-05-05 17:50:47 -0400

    copyright changes

--- js/index.js
+++ js/index.js
@@ -4,3 +4,16 @@ const newCopyright = document.createElement('div');
 //create a variable named today and assign it a new date object
 const today = new Date();
 
+const thisYear = today.getFullYear();
+
+
+//add new footer element to the body document
+document.body.appendChild(newCopyright);
+
+const footer = document.querySelector('#Connect');
+
+const copyright = document.createElement('p');
+
+copyright.innerHTML = `© ${thisYear} Molly Brown`;
+
+footer.appendChild(copyright);
